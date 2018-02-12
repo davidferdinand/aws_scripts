1325f26e44a79a50de7f7559e67d86940b6b54ad
davidferdinand <david.ferdinand@bglgroup.co.uk>
2018-02-12 14:04:16 +0000

    updated key script

--- keys90days.py
+++ keys90days.py
@@ -1,26 +1,23 @@
-# This script will result all the AWS IAM Users whose access keys are 90 days old
-
 import datetime
+import pytz
 import dateutil
 from dateutil import parser
+from dateutil.tz import tzutc
 
-import boto
-from boto import iam
+import boto3
 
+conn = boto3.client('iam')
+resource = boto3.resource('iam')
 
-conn=iam.connect_to_region('eu-west-1')
-users=conn.get_all_users()
-timeLimit=datetime.datetime.now() - datetime.timedelta(days=90)
+timeLimit=datetime.datetime.now(tzutc()) - datetime.timedelta(days=90)
 
 print "---------------------------------------------"
-print "Access Keys Date" + "\t\t" + "Username"
+print "UserName" + "\t\t" + "Access Created Date"
 print "---------------------------------------------"
 
-for user in users.list_users_response.users:
-
-    accessKeys=conn.get_all_access_keys(user_name=user['user_name'])
-    for keysCreatedDate in accessKeys.list_access_keys_response.list_access_keys_result.access_key_metadata:
-
-        if parser.parse(keysCreatedDate['create_date']).date() <= timeLimit.date():
-
-            print(keysCreatedDate['create_date']) + "\t\t" + user['user_name']
+for user in resource.users.all():
+    Metadata = conn.list_access_keys(UserName=user.user_name)
+    for key in Metadata['AccessKeyMetadata']:
+        if 'CreateDate' in key:
+            if key['CreateDate'] <= timeLimit:
+                print key['UserName'], "\t\t" , key['CreateDate']
